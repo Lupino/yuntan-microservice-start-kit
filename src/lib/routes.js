79217a13c182008c1a3da939118a1c8bb622edfe
Lupino <lmjubuntu@gmail.com>
2017-11-27 13:27:08 +0800

    add create artile api

---
+++ src/lib/routes.js
@@ -0,0 +1,22 @@
+import {requireLogin, requireAdmin, userSrv, getUser} from './user';
+import {sendJsonResponseWithPromise} from '../utils';
+
+import {article as articleConfig} from '../../config';
+import {ArticleService} from 'yuntan-service';
+
+export const articleSrv = new ArticleService(articleConfig);
+
+export function route(app) {
+  app.post('/api/create_article/', requireAdmin(), (req, res) => {
+    async function doCreateArticle() {
+      const {title, summary, content} = req.body;
+      const user = req.currentUser;
+      let art = await articleSrv.create({title, summary, content});
+      await articleSrv.createTimeline('user_' + user.id, art.id);
+      art.timelines = ['user_' + user.id];
+      return art;
+    }
+    sendJsonResponseWithPromise(res, doCreateArticle, {spec: 'article'})();
+  });
+}
+
